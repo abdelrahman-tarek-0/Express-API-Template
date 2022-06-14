d43e951d53f576a708ddfd96f781c65c615fb4d7
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-06-14 12:05:33 +0200

    update: added morgan , helmet for security and limit for how many requests in min

---
+++ API/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const expressRateLimit = require('express-rate-limit');
+// const error_handler_middleware = require('../middleware/error.middleware');
+
+
+// config the server 
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(expressRateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+	legacyHeaders: false
+}));
+app.use(helmet());
+app.use(express.json());
+app.use(cors())
+
+app.use('/api', (req,res)=>{
+    res.send('Hello World');
+});
+
+//error handling
+// app.use(error_handler_middleware);
+app.use((_req, res) => {
+    res.status(404).json({
+        message: 'API route not found (check the documentation)',
+    });
+});
+
+
+app.listen(port,()=>{
+    console.log("server is running")
+    console.log(`on localhost:${port}`)
+})
