9529d0beb6e4b528b2f82953332d8472c8dc4eb6
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-06-14 13:33:11 +0200

    udate: added prettier and eslint config

--- API/src/index.js
+++ API/src/index.js
@@ -1,42 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const expressRateLimit = require('express-rate-limit');
-const error_handler_middleware = require('../middleware/error.middleware.js');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const expressRateLimit = require('express-rate-limit')
+const errorHandlerMiddleware = require('../middleware/error.middleware.js')
 
+// config the server
+const app = express()
+const port = 3000
 
-// config the server 
-const app = express();
-const port = 3000;
-
-app.use(morgan('dev'));
-app.use(expressRateLimit({
+app.use(morgan('dev'))
+app.use(
+  expressRateLimit({
     windowMs: 30 * 1000,
     max: 15,
     standardHeaders: true,
-	legacyHeaders: false
-}));
+    legacyHeaders: false,
+  })
+)
 
-app.use(helmet());
-app.use(express.json());
+app.use(helmet())
+app.use(express.json())
 app.use(cors())
 
-app.get('/api', (req,res)=>{
-    res.send('Hello World');
-});
+app.get('/api', (req, res) => {
+  res.send('Hello World')
+})
 
-
-//error handling
-app.use(error_handler_middleware);
+// error handling
+app.use(errorHandlerMiddleware)
 app.use((_req, res) => {
-    res.status(404).json({
-        message: 'API route not found (check the documentation)',
-    });
-});
+  res.status(404).json({
+    message: 'API route not found (check the documentation)',
+  })
+})
 
-
-app.listen(port,()=>{
-    console.log("server is running")
-    console.log(`on localhost:${port}`)
+app.listen(port, () => {
+  console.log('server is running')
+  console.log(`on localhost:${port}`)
 })
